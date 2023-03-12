38272a2cc28d438579b5fda00103c56de02df9ff
Oluwamayowa247 <mayowaolutuyo@gmail.com>
2023-03-13 00:46:23 +0100

    xoxo 😄

--- lib/widgets/chat_list.dart
+++ lib/widgets/chat_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/info.dart';
+import 'package:whatsapp_clone/widgets/sender_message_card.dart';
 
 class ChatList extends StatelessWidget {
   const ChatList({super.key});
@@ -9,9 +10,12 @@ class ChatList extends StatelessWidget {
     return ListView.builder(
       itemCount: messages.length,
       itemBuilder: (BuildContext context, int index) {
-        if(messages[index]["isMe"]== true){
-          //My message card 
-          return Text('Faruq is mad');
+        if (messages[index]["isMe"] == true) {
+          //My message card
+          return SenderMessageCard(
+            messages: messages[index]['text'].toString(),
+            time: messages[index]['time'].toString(),
+          );
         }
         //sender card
         return Text('Wunmi is mad');
